026c56e2d218004336ef0c59846c6a6904b6ab77
Gijs <gijs.muys@student.kdg.be>
2013-03-05 23:25:21 +0100

    check if image is right content type

--- Model/src/main/java/be/kdg/trips/persistence/dao/impl/UserDaoImpl.java
+++ Model/src/main/java/be/kdg/trips/persistence/dao/impl/UserDaoImpl.java
@@ -4,6 +4,7 @@ import be.kdg.trips.exception.TripsException;
 import be.kdg.trips.model.user.User;
 import be.kdg.trips.persistence.dao.interfaces.UserDao;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
