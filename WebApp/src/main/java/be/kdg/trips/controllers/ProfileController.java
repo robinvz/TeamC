cf3f262f3d433097b0fe800c8485a5bd4f1483e6
Gijs <gijs.muys@student.kdg.be>
2013-03-13 10:33:26 +0100

    editQuestion

--- WebApp/src/main/java/be/kdg/trips/controllers/ProfileController.java
+++ WebApp/src/main/java/be/kdg/trips/controllers/ProfileController.java
@@ -1,7 +1,7 @@
 package be.kdg.trips.controllers;
 
 import be.kdg.trips.beans.LoginBean;
-import be.kdg.trips.exception.TripsException;
+import be.kdg.trips.businessLogic.exception.TripsException;
 import be.kdg.trips.model.user.User;
 import be.kdg.trips.services.interfaces.TripsService;
 import org.springframework.beans.factory.annotation.Autowired;
